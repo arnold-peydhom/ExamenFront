17955350093dadce95d29820b63971703ab1d649
arnoldpeydhom <arnoldpeydhom276@gmail.com>
2025-01-26 21:28:20 +0100

    creation des differents composant de chaque module et exportation de ces composants dans le module associe au composant

--- gestion_cours/src/app/course/course.module.ts
+++ gestion_cours/src/app/course/course.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { CourseDetailsComponent } from './course-card/course-details/course-details.component';
+import { CourseComponent } from './service/course/course.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    CourseCardComponent,
+    CourseDetailsComponent,
+    CourseComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomeComponent,
+    CourseCardComponent,
+    CourseDetailsComponent
   ]
 })
 export class CourseModule { }
